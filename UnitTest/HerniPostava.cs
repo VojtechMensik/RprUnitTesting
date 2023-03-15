d6cf946bcae58914ff16eeebdd3747dcbffc4492
KrivanTomas <114081566+KrivanTomas@users.noreply.github.com>
2023-03-15 13:09:21 +0100

    hodina1 (╯°□°）╯︵ ┻━┻
    (merci to umi)

---
+++ UnitTest/HerniPostava.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    public class HerniPostava
+    {
+        private string jmeno;
+        public string Jmeno {
+            get
+            {
+                return this.jmeno;
+            }
+            set
+            {
+                if (value.Length > 10) throw new Exception("NameLength");
+                this.jmeno = value;
+            }
+        }
+
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public int level;
+        public HerniPostava(string jmeno)
+        {
+            this.Jmeno = jmeno;
+            this.level = 1;
+        }
+
+        public void ZmenaPozice(double x, double y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public override string ToString()
+        {
+            return String.Format(
+                "Jmeno: {0}\n" +
+                "Level: {1}\n" +
+                "X: {2}, Y: {4}",
+                Jmeno, level, X, Y);
+        }
+    }
+}
---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,66 @@
+using System;
+using NUnit.Framework;
+using UnitTest;
+
+namespace UnitTestProject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void MocDloheJmeno()
+        {
+            Assert.Throws<Exception>(() =>
+            {
+                new HerniPostava("tohlejemocdlouhe");
+            });
+        }
+
+        [Test]
+        public void SpravneJmeno()
+        {
+            Assert.DoesNotThrow(() =>
+            {
+                new HerniPostava("kratke");
+            });
+        }
+
+        [Test]
+        public void PocatecniLevel()
+        {
+            Assert.IsTrue(new HerniPostava("Luna").level == 1);
+        }
+
+        [Test]
+        public void PocatecniX()
+        {
+            Assert.IsTrue(new HerniPostava("Misha").X == 0);
+        }
+
+        [Test]
+        public void PocatecniY()
+        {
+            Assert.IsTrue(new HerniPostava("Muffin").Y == 0);
+        }
+
+        [Test]
+        public void ZmenaSouradnic()
+        {
+            HerniPostava postava = new HerniPostava("Oliver");
+            double ocekavanyX = 69;
+            double ocekavanyY = 420;
+            postava.ZmenaPozice(ocekavanyX, ocekavanyY);
+            Assert.IsTrue(postava.X == ocekavanyX && postava.Y == ocekavanyY);
+        }
+    }
+}
