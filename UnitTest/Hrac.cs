f6cc3e6de4aca21c43035437016ee831fc42d925
VojtechMensik <mensikv.03@spst.eu>
2023-03-29 12:16:02 +0200

    Hrac,NPC
    naprogramování

--- UnitTest/Hrac.cs
+++ UnitTest/Hrac.cs
@@ -3,11 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms.VisualStyles;
 
 namespace UnitTest
 {
-    class Hrac
+    public class Hrac : HerniPostava
     {
+        public Hrac(BarvaVlasu barvaVlasu, Oblicej oblicej, Vlasy vlasy, string specializace) 
+        { 
+        
+        }
+        private string specializace;
+        public string Specializace
+        {
+            get { return specializace; }
+            set
+            {
+                if (value != "Kouzelník" && value != "Berserker" && value != "Inženýr" && value != "Cizák")
+                    specializace = value;
+            }
+        }
+        public enum BarvaVlasu { Hneda, Cerna, Blond, Zrzava };
+        public enum Oblicej { VelkyNos, Usoplesk, MakeUp }
+        public enum Vlasy { Drdol, Culik, Pleska};
 
+        public BarvaVlasu barvaVlasu;
+        public Oblicej oblicej;
+        public Vlasy vlasy;
+
+        public int XP { get; private set; }
+
+        public void PridejXP(int xp)
+        {
+            XP += xp;
+            if (XP > Level * 100)
+            {
+                XP -= Level * 100;
+                Level++;
+            }
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + string.Format(" Specializace - {0} BarvaVlasu - {1} Oblicej - {2} Vlasy - {3} XP - {4}",Specializace,barvaVlasu,oblicej,vlasy,XP);
+        }
     }
 }
---
+++ UnitTest/NPC.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    public class NPC : HerniPostava
+    {
+        public NPC(PopisPrace popisPrace, bool sila) 
+        { 
+            PopisNPC = popisPrace;
+            Sila= sila;
+        }
+        public NPC(PopisPrace popisPrace)
+        {
+            PopisNPC = popisPrace;
+        }
+        public enum PopisPrace { obchodnik, nepritel, obyvatel };
+        public PopisPrace PopisNPC { get; private set; }
+        /// <summary>
+        /// 1 == boss 0 == neni boss
+        /// </summary>
+        public bool Sila { get; private set; }
+
+        public override void ZmenaPozice(int x, int y)
+        {
+            return;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + string.Format("PopisNPC - {0} {1}",PopisNPC,(Sila?"Boss":"Minion"));
+        }
+    }
+}
